7fb97bf958c35153cca2197505317f70f6a4faa0
Tirth-2005 <tirthpanchal2825@gmail.com>
2024-10-12 08:10:54 +0530

    add-on commit

--- happy_garden.py
+++ happy_garden.py
@@ -63,6 +63,7 @@ def game_loop():
         actor_img = pygame.transform.scale(actor_img, (80, 100))
         actor_water = game_images['pig_water']
         actor_water = pygame.transform.scale(actor_water, (100, 100))
+        actor = actor_img.get_rect(topleft=(100, 500))
         
     # cow_img = game_images['cow']
     # cow = cow_img.get_rect(topleft=(100, 500))
